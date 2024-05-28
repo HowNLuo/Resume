1271b1118685282f880509fd4d58ee36c6e5e7b6
Win.Luo <Win.Luo@infotech.com.tw>
2024-05-28 15:30:11 +0800

    新增element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import './index.scss'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 import { router } from './routes'
 
-const app = createApp(App).use(router)
+const app = createApp(App).use(router).use(ElementPlus)
 
 app.mount('#app')
