7f361ed8fcb2764cfbf73438e97cbff0dd148565
Win.Luo <xji6cl4p@gmail.com>
2024-06-17 11:44:12 +0800

    新增Footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <Header />
-  <div class="bg-gray-100 h-full py-[90px]">
+  <div class="bg-gray-100 min-h-screen py-[90px]">
     <router-view />
   </div>
+  <Footer />
 </template>
 
 <script lang="ts" setup>
 import Header from './views/template/Header.vue';
+import Footer from './views/template/Footer.vue';
 </script>
---
+++ src/views/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-primary-100 h-[100px] flex flex-col gap-2 items-center justify-center text-gray-700">
+    <div>Designed by Win.Luo</div>
+    <div class="flex gap-4 text-gray-700 text-lg">
+      <a href="https://www.facebook.com/HowNLuo/" class="hover:text-primary-500">
+        <font-awesome-icon :icon="['fab', 'facebook']" />
+      </a>
+      <a href="https://www.instagram.com/luohaoen1229/?hl=zh-tw" class="hover:text-primary-500">
+        <font-awesome-icon :icon="['fab', 'instagram']" />
+      </a>
+      <a href="https://line.me/ti/p/0t-3nLcwLo" class="hover:text-primary-500">
+        <font-awesome-icon :icon="['fab', 'line']" />
+      </a>
+      <a href="mailto:xji6cl4p@gmail.com" class="hover:text-primary-500">
+        <font-awesome-icon :icon="['fas', 'envelope']" />
+      </a>
+      <a href="https://github.com/HowNLuo" class="hover:text-primary-500">
+        <font-awesome-icon :icon="['fab', 'github']" />
+      </a>
+    </div>
+    <div>© 2024 Win.Luo. All rights reserved.</div>
+  </div>
+</template>
